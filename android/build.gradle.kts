712e4ed9eee107b1b5ba7073cbb15f84c5af2d6a
Tom Ha <tomh@apadmi.com>
2026-06-01 15:39:11 +0100

    chore: Bump Kotlin version

---
+++ android/build.gradle.kts
@@ -0,0 +1,81 @@
+group = "com.apadmi.flutter_security_suite"
+version = "1.0-SNAPSHOT"
+
+buildscript {
+    val kotlinVersion = "2.3.20"
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:9.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.apadmi.flutter_security_suite"
+
+    compileSdk = 36
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.directories += "src/main/kotlin"
+        }
+        getByName("test") {
+            java.directories += "src/test/kotlin"
+        }
+    }
+
+    defaultConfig {
+        minSdk = 23
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            all {
+                it.useJUnitPlatform()
+
+                it.outputs.upToDateWhen { false }
+
+                it.testLogging {
+                    events("passed", "skipped", "failed", "standardOut", "standardError")
+                    showStandardStreams = true
+                }
+            }
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    implementation("com.scottyab:rootbeer-lib:0.1.2")
+    testImplementation("org.mockito:mockito-core:5.23.0")
+}
